d89094e769a9e61e310acd526a4c304bf64e8677
rejie0224 <tyfun3035@gmail.com>
2020-10-28 11:48:20 +0800

    init comment

---
+++ Assets/Scripts/MultiCameraController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using unity4dv;
+using UnityEngine.Rendering;
+
+public class MultiCameraController : MonoBehaviour
+{
+    public float initAngle = -90.0f;
+    public float radius = 3.5f;
+    public Plugin4DS fakeScene;
+    public Camera mainCamera;
+
+    private Camera[] cameras;
+    private RenderTexture[] renderTextures;
+    private int activeCameraID = 0;
+
+    void Awake()
+    {
+        RenderPipelineManager.endFrameRendering += RenderPipelineManager_endFrameRendering;
+
+        cameras = GetComponentsInChildren<Camera>();
+        renderTextures = new RenderTexture[cameras.Length];
+
+        float angleStep = 360 / cameras.Length;
+        float currentAngle = initAngle;
+        for(int i = 0; i < cameras.Length; ++i)
+        {
+            float rad = currentAngle * Mathf.Deg2Rad;
+            Vector3 newPosition = new Vector3(radius * Mathf.Cos(rad), 0.0f, radius * Mathf.Sin(rad)) + this.transform.position;
+            cameras[i].transform.position = newPosition;
+            cameras[i].transform.rotation = Quaternion.LookRotation(this.transform.position - newPosition, Vector3.up);
+            currentAngle += angleStep;
+
+            renderTextures[i] = new RenderTexture(960, 540, 24, RenderTextureFormat.ARGB32);
+            renderTextures[i].depth = 0;
+            renderTextures[i].Create();
+
+            cameras[i].targetTexture = renderTextures[i];
+        }
+
+    }
+
+    private void RenderPipelineManager_endFrameRendering(ScriptableRenderContext arg1, Camera[] arg2)
+    {
+        Graphics.Blit(renderTextures[activeCameraID], null as RenderTexture);
+        //mainCamera.Render();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            activeCameraID = (activeCameraID + 1) % cameras.Length;
+            SetCameraEnable(activeCameraID);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            activeCameraID = (activeCameraID - 1 + cameras.Length) % cameras.Length;
+            SetCameraEnable(activeCameraID);
+        }
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(fakeScene.IsPlaying)
+            {
+                fakeScene.Play(false);
+            }
+            else
+            {
+                fakeScene.Play(true);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            SaveScreenShot();
+        }
+    }
+
+    void SetCameraEnable(int cameraID)
+    {
+        activeCameraID = Math.Min(Math.Max(cameraID, 0), cameras.Length - 1);
+        //for(int i = 0; i < cameras.Length; ++i)
+        //{
+        //    cameras[i].enabled = false;
+        //}
+        //mainCamera.transform.position = cameras[activeCameraID].transform.position;
+        //mainCamera.transform.rotation = cameras[activeCameraID].transform.rotation;
+    }
+
+    void SaveScreenShot()
+    {
+        for(int i = 0; i < cameras.Length; ++i)
+        {
+            SaveRenderTextureToFile(cameras[i].targetTexture, i.ToString());
+        }
+    }
+
+    private void SaveRenderTextureToFile(RenderTexture rt, string fileName)
+    {
+        RenderTexture.active = rt;
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+
+        byte[] bytes;
+        bytes = tex.EncodeToPNG();
+
+        string path = Application.streamingAssetsPath + "/" + fileName + ".png";
+        System.IO.File.WriteAllBytes(path, bytes);
+    }
+}
