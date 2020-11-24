d38b8d470b5f946e8c481def6c629682e8e09f4f
賓 <bing5300@gmail.com>
2020-11-24 15:03:30 +0800

    camera.json now save file array & put save file to another thread

--- Assets/Scripts/MultiCameraController.cs
+++ Assets/Scripts/MultiCameraController.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using unity4dv;
 using UnityEngine.Rendering;
+using System.Threading;
 
 [System.Serializable]
-public class CameraParamList{
+public class CameraParamList {
     public List<CameraParam> camera = new List<CameraParam>();
 }
 
 [System.Serializable]
-public class CameraParam{
+public class CameraParam {
     public int id;
-    public string img;
+    public string[] img;
     public Matrix4x4 world2screenMat;
     public Vector3 pos;
     public Quaternion quat;
 
 }
 
-public class MultiCameraController : MonoBehaviour
-{
+public class MultiCameraController : MonoBehaviour {
     public float initAngle = -90.0f;
     public float radius = 3.5f;
     public Plugin4DS fakeScene;
@@ -34,10 +34,12 @@ public class MultiCameraController : MonoBehaviour
     private bool isRecord = false;
     private const int recordLen = 10;
     private int curRecordFrame = 0;
+    private bool isSaving = false;
+
+    private List<List<byte[]>> camData;
 
 
-    void Awake()
-    {
+    void Awake() {
         RenderPipelineManager.endFrameRendering += RenderPipelineManager_endFrameRendering;
 
         cameras = GetComponentsInChildren<Camera>();
@@ -45,8 +47,10 @@ public class MultiCameraController : MonoBehaviour
 
         float angleStep = 360 / cameras.Length;
         float currentAngle = initAngle;
-        for(int i = 0; i < cameras.Length; ++i)
-        {
+
+        camData = new List<List<byte[]>>();
+
+        for (int i = 0; i < cameras.Length; ++i) {
             float rad = currentAngle * Mathf.Deg2Rad;
             Vector3 newPosition = new Vector3(radius * Mathf.Cos(rad), 0.0f, radius * Mathf.Sin(rad)) + this.transform.position;
             cameras[i].transform.position = newPosition;
@@ -58,64 +62,81 @@ public class MultiCameraController : MonoBehaviour
             renderTextures[i].Create();
 
             cameras[i].targetTexture = renderTextures[i];
+            camData.Add(new List<byte[]>());
         }
+
+
 
     }
 
-    private void RenderPipelineManager_endFrameRendering(ScriptableRenderContext arg1, Camera[] arg2)
-    {
+    private void RenderPipelineManager_endFrameRendering(ScriptableRenderContext arg1, Camera[] arg2) {
         Graphics.Blit(renderTextures[activeCameraID], null as RenderTexture);
         //mainCamera.Render();
     }
 
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             activeCameraID = (activeCameraID + 1) % cameras.Length;
             SetCameraEnable(activeCameraID);
-        }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
-        {
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
             activeCameraID = (activeCameraID - 1 + cameras.Length) % cameras.Length;
             SetCameraEnable(activeCameraID);
-        }
-        else if(Input.GetKeyDown(KeyCode.P))
-        {
-            if(fakeScene.IsPlaying)
-            {
+        } else if (Input.GetKeyDown(KeyCode.P)) {
+            if (fakeScene.IsPlaying) {
                 fakeScene.Play(false);
-            }
-            else
-            {
+            } else {
                 fakeScene.Play(true);
             }
-        }
-        else if(Input.GetKeyDown(KeyCode.S) && !isRecord)
-        {
+        } else if (Input.GetKeyDown(KeyCode.S) && !isRecord) {
             isRecord = true;
             curRecordFrame = 0;
         }
 
         //handle record
         if (isRecord) {
-            //record
-            SaveScreenShot(curRecordFrame);
+
+            //check is record finish
+            if (curRecordFrame >= recordLen) {
+                isRecord = false;
+                //save screen shot
+                new Thread(SaveScreenShot).Start();
+                //save json
+                SaveJson(recordLen);
+                return;
+            }
+            cacheScreenShot();
             //check record finish
             curRecordFrame++;
-            if (curRecordFrame >= recordLen)
-                isRecord = false;
+
         }
     }
 
-    void SetCameraEnable(int cameraID)
-    {
+    void SaveJson(int len) {
+        CameraParamList list = new CameraParamList();
+
+        for (int i = 0; i < cameras.Length; ++i) {
+            CameraParam cam = new CameraParam();
+            cam.world2screenMat = cameras[i].projectionMatrix * cameras[i].worldToCameraMatrix;
+            cam.id = i;
+            cam.pos = cameras[i].transform.position;
+            cam.quat = cameras[i].transform.rotation;
+            cam.img = new string[len];
+            for (int frame = 0; frame < len; frame++) {
+                cam.img[frame] = "/" + i.ToString() + "_" + frame + ".png";
+            }
+            list.camera.Add(cam);
+        }
+
+        string camInfo = JsonUtility.ToJson(list);
+        System.IO.File.WriteAllText(Application.streamingAssetsPath + "/camera.json", camInfo);
+    }
+
+    void SetCameraEnable(int cameraID) {
         activeCameraID = Math.Min(Math.Max(cameraID, 0), cameras.Length - 1);
         //for(int i = 0; i < cameras.Length; ++i)
         //{
@@ -125,32 +146,22 @@ public class MultiCameraController : MonoBehaviour
         //mainCamera.transform.rotation = cameras[activeCameraID].transform.rotation;
     }
 
-    void SaveScreenShot(int curFrame)
-    {
-
-        CameraParamList list= new CameraParamList();
-
-        for(int i = 0; i < cameras.Length; ++i)
-        {
-            CameraParam cam = new CameraParam();
-            cam.world2screenMat = cameras[i].projectionMatrix  * cameras[i].worldToCameraMatrix;
-            cam.id = i;
-            cam.pos = cameras[i].transform.position;
-            cam.quat = cameras[i].transform.rotation;
-
-            cam.img = "/"+i.ToString()+"_" + curFrame + ".png";
-            string imgPath = SaveRenderTextureToFile(cameras[i].targetTexture, cam.img);
-            Debug.Log(imgPath);
-            list.camera.Add(cam);
+    void cacheScreenShot() {
+        for (int i = 0; i < cameras.Length; ++i) {
+            camData[i].Add(saveRenderTextureToByte(cameras[i].targetTexture));
         }
-
-        string camInfo = JsonUtility.ToJson(list);
-        System.IO.File.WriteAllText(Application.streamingAssetsPath + "/camera.json", camInfo);  
-
     }
 
-    private string SaveRenderTextureToFile(RenderTexture rt, string fileName)
-    {
+    void SaveScreenShot() {
+        for (int camIndex = 0; camIndex < camData.Count; camIndex++) {
+            for (int dataIndex = 0; dataIndex < camData[camIndex].Count; dataIndex++) {
+                string imgPath = saveByteToFile(camData[camIndex][dataIndex], "/" + camIndex.ToString() + "_" + dataIndex + ".png");
+                Debug.Log(imgPath);
+            }
+        }
+    }
+
+    private string SaveRenderTextureToFile(RenderTexture rt, string fileName) {
         RenderTexture.active = rt;
         Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
         tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
@@ -161,6 +172,23 @@ public class MultiCameraController : MonoBehaviour
 
         string path = Application.streamingAssetsPath + fileName;
         System.IO.File.WriteAllBytes(path, bytes);
+        return path;
+    }
+
+    private byte[] saveRenderTextureToByte(RenderTexture rt) {
+        RenderTexture.active = rt;
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+
+        byte[] bytes;
+        bytes = tex.EncodeToPNG();
+        return bytes;
+    }
+
+    private string saveByteToFile(byte[] data, string fileName) {
+        string path = Application.streamingAssetsPath + fileName;
+        System.IO.File.WriteAllBytes(path, data);
         return path;
     }
 }
